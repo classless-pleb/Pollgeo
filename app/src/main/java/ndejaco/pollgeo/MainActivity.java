8411fbc943697de11edfe93b292f31152b485314
ndejaco <ndejaco@email.arizona.edu>
2015-09-20 13:32:35 -0700

    Initial change

--- app/src/main/java/ndejaco/pollgeo/MainActivity.java
+++ app/src/main/java/ndejaco/pollgeo/MainActivity.java
@@ -7,10 +7,13 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Test
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     @Override
