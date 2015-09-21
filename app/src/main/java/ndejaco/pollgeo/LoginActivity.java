7309b573d0f35b7750a6c43af854bad3e04b9ad9
classless-pleb <znixon@email.arizona.edu>
2015-09-21 15:14:28 -0700

    Added Parse - 2nd try

--- app/src/main/java/ndejaco/pollgeo/LoginActivity.java
+++ app/src/main/java/ndejaco/pollgeo/LoginActivity.java
@@ -7,6 +7,7 @@ import android.view.MenuItem;
 
 public class LoginActivity extends AppCompatActivity {
 
+    //Test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
